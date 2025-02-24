bb79da144120f795d77806c9647fb4e52f947e5e
juanchang23301 <cha23301@uvg.edu.gt>
2025-02-24 00:52:18 -0600

    Update main.asm

--- Lab03 - Chang - 23301/Lab03 - Chang - 23301/main.asm
+++ Lab03 - Chang - 23301/Lab03 - Chang - 23301/main.asm
@@ -21,6 +21,7 @@
 .org 0x0020					; Vector de interrupción para TIMER0 OVF
 	RJMP TIMER0				; Saltar a la rutina a la subrrutina 
 
+
 START:
 ;Confifurar LEDs
     CLI                     ; Deshabilitar interrupciones mientras se configurax
@@ -30,7 +31,7 @@ START:
     LDI R16, 0x00
     OUT DDRB, R16           ; Configurar PB0 y PB1 como entrada
 
-    LDI R16, 0x03
+    LDI R16, 0x0F
     OUT PORTB, R16          ; Habilitar pull-ups en PB0 y PB1
 
 	LDI R16, (1 << PCIE0)   ;  Habilitar PCINT en PORTB
@@ -52,9 +53,11 @@ START:
 
     SEI                     ; Habilitar interrupciones globales
 
+	CLR R17					; Inicializar contador de LEDs
 	CLR R20					; Inicializar contador del display
     CLR R21					; Inicializar contador de overflow
-    CLR R17					; Inicializar contador de LEDs
+	CLR R22                 ; Inicializar contador de decenas
+	CLR R23                 ; Flag de multiplexación
     RCALL UPDATE_LEDS       ; Actualizar LEDs con valor inicial
 
 MAIN_LOOP:
@@ -97,15 +100,9 @@ TIMER0:
 	LDI R16, 100
     OUT TCNT0, R16
 
-    INC R21					; Incrementar contador de 10ms
-    CPI R21, 100			; ¿Llegó a 100 (1 segundo)?
-    BRNE END_ISR
-
-    CLR R21					; Reiniciar contador de 10ms
-    INC R20					; Incrementar segundos
-    CPI R20, 10				; Si R20 alcanza 10, reiniciarlo a 0
-    BRNE UPDATE_DISPLAY
-    CLR R20
+	LDI R16, 0x01
+    EOR R23, R16			; Alternar bandera de multiplexación
+    BREQ UPDATE_DISPLAY2	; Si R23 = 0, mostrar decenas
 
 UPDATE_DISPLAY:				
 	LDI ZH, 0X01			; Cargar parte alta de la tabla
@@ -113,13 +110,43 @@ UPDATE_DISPLAY:
     ADD ZL, R20				; Calcular dirección de patrón en SRAM
     LD R16, Z				; Cargar patrón desde SRAM
     OUT PORTD, R16			; Mostrar en el display
-   
+	SBI PORTC, 5			; Encender display de decenas (PC5)
+    CBI PORTC, 4			; Apagar display de unidades (PC4)
+
+	RJMP CHECK_COUNT
+
+UPDATE_DISPLAY2:
+    LDI ZH, 0X01           ; Cargar parte alta de la tabla
+    LDI ZL, 0X00           ; Cargar parte baja de la tabla
+    ADD ZL, R22            ; Calcular dirección de patrón con R22
+    LD R16, Z              ; Cargar patrón desde SRAM
+    OUT PORTD, R16         ; Mostrar en el display
+	SBI PORTC, 4           ; Encender display de unidades (PC4)
+    CBI PORTC, 5           ; Apagar display de decenas (PC5)
+
+CHECK_COUNT:
+	INC R21					; Incrementar contador de 10ms
+    CPI R21, 100			; ¿Llegó a 100 (1 segundo)?
+    BRNE END_ISR
+
+    CLR R21					; Reiniciar contador de 10ms
+    INC R20					; Incrementar segundos
+	CPI R20, 10             ; ¿Llegó a 10?
+    BRNE END_ISR
+
+    CLR R20                 ; Reiniciar contador de segundos
+	INC R22					; Incrementar contador de decenas
+    CPI R22, 6				; Si llega a 6 (60 segundos)
+    BRNE END_ISR
+    CLR R22  
+
+		
 END_ISR:
     POP R16					; Restaurar SREG
     OUT SREG, R16
     POP R16					; Restaurar R16
-							; Retorno de la interrupción
-	RETI
+	RETI					; Retorno de la interrupción
+	
 ;==========================================================================
 
 TABLE:
